756b6c7b4cd340953f73868a035696b302fd562f
james <jamesdeleon373@gmail.com>
2023-03-28 18:41:49 +0800

    Finished play again button. Added style for the play again button

--- app.js
+++ app.js
@@ -10,32 +10,75 @@ const scissors = document.getElementById('scissors');
 
 const resultBanner = document.createElement('h1');
 const container = document.getElementById('result');
+const reset = document.getElementById('resetButton');
 
 
-const playerTracker = document.getElementById('playerScore');
-const computerTracker = document.getElementById('computerScore');
+
+
+
+
 
 container.append(resultBanner);
 
+    //function to show the "play again button"
+    function showReset () {
+        reset.style.visibility = 'visible';
+    }
+
+    // resets the score of the game, hides the "play again" button and clears the text in the result banner
+    function resetGame () {
+        playerScore = 0;
+        computerScore = 0;
+        reset.style.visibility = 'hidden';
+        resultBanner.textContent = '';
+        
+        updateScore();
+    }
+
+    // a function to track the score of the player and the computer
+    function updateScore (result) {
 
 
+        if (result === 1) {
+            playerScore++;
+        }
 
+        if (result === 2) {
+            computerScore++;
+        } 
 
+        const playerTracker = document.getElementById('playerScore');
+        const computerTracker = document.getElementById('computerScore');
 
+        playerTracker.textContent = `Player: ${playerScore}`;
+        computerTracker.textContent = `Computer: ${computerScore}`;
 
+        
+        if (playerScore || computerScore === 5 ) {
+            
+            if (playerScore === 5) {
+                resultBanner.textContent = 'Game over! Congratulations, you win!';
+                showReset();
 
+            } else if (computerScore === 5) {
+                resultBanner.textContent = 'Game over! Ai wins! Try again next time';
+                showReset();
 
+            }
+        
+        }
+ 
+    }
 
+    // generates a random computer choice between 0 to 2 
 
-
-
-    
     function getComputerChoice () {
 
         // Rock = 0, Paper = 1, Scissors = 2
         return  Math.floor(Math.random() * 3);
     }
 
+    // plays a round of rock, paper, scissors based on the player's choice
     function playRound (playerSelection, computerSelection) {
         
         // result = 0 -> draw , result = 1 -> win, result = 2 -> lose
@@ -50,45 +93,37 @@ container.append(resultBanner);
             switch(playerSelection) {
                 case 0:
                     resultBanner.textContent = 'You win, Rock beats Scissors';
-                    console.log('You win, Rock beats Scissors!');
                     break;
                 case 1:
                     resultBanner.textContent = 'You win, Paper beats Rock!';
-                    console.log('You win, Paper beats Rock!');
                     break;
                 case 2:
                     resultBanner.textContent = 'You win, Scissors beats Paper!';
-                    console.log('You win, Scissors beats Paper!');
                     break;
                 default:
                     console.log("Error");
             }
 
-
-
-            
-            return 1;
+             return 1;
 
         }else if (result === 2) {
 
             switch(playerSelection) {
                 case 0:
                     resultBanner.textContent = 'Ai wins, Paper beats Rock!';
-                    console.log('Ai wins, Paper beats Rock!');
                     break;
                 case 1:
                     resultBanner.textContent = "Ai wins, Scissors beats Paper!";
-                    console.log('Ai wins!, Scissors beats Paper!');
                     break;
                 case 2:
                     resultBanner.textContent = "Ai wins, Rock beats Scissors!";
-                    console.log('Ai wins!, Rock beats Scissors!!');
                     break;
                 default:
                     console.log("Error");
             }
 
             return 2;
+
         } else {
             console.log("Draw");
             return 0;
@@ -97,50 +132,34 @@ container.append(resultBanner);
 
     }
 
+    function getWinner () {
+
+        
+
+    }
+
 
 
 rock.addEventListener('click', () => {
     let result = playRound(0, getComputerChoice());
-
-    if ( result === 1 ){
-        playerScore += 1;
-    }else if (result === 2) {
-        computerScore += 1;
-    }
-
-    playerTracker.textContent += playerScore;
-    computerTracker.textContent += computerScore;
-
+    updateScore(result);
 
 })
 
 paper.addEventListener('click', () => {
      let result = playRound(1, getComputerChoice());
+    updateScore(result);
 
-     if ( result === 1 ){
-        playerScore += 1;
-    }else if (result === 2) {
-        computerScore += 1;
-    }
-
-    playerTracker.textContent += playerScore;
-    computerTracker.textContent += computerScore;
-     
 })
 
 scissors.addEventListener('click', () => {
      let result = playRound(2, getComputerChoice());
+    updateScore(result);
 
-     if ( result === 1 ){
-        playerScore += 1;
-    }else if (result === 2) {
-        computerScore += 1;
-    }
+})
 
-    playerTracker.textContent += playerScore;
-    computerTracker.textContent += computerScore;
-
-     
+reset.addEventListener('click', () => {
+    resetGame();
 })
 
 
